79f431609bc7072f5e73123d7cc7effd5bb85d4c
crystal <crystal@joymason.cn>
2016-08-16 10:55:09 +0800

    no message

---
+++ FadeTableView/Config.h
@@ -0,0 +1,21 @@
+//
+//  Config.h
+//  FadeTableView
+//
+//  Created by Crystal on 16/8/8.
+//  Copyright © 2016年 crystal. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+//#define CRYSTAL_DEBUG
+#define CRYSTAL_ONLINE
+
+#ifdef CRYSTAL_DEBUG
+#define CRYSTALNAME @"crystal_debug"
+#else
+#define CRYSTALNAME @"crystal_PE"
+#endif
+
+#endif /* Config_h */
